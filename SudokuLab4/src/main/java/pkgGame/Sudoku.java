cf822408a4832b56b366034a4e8d258964b05625
runne <runne@LAPTOP-VNRD7H0F.home>
2018-10-04 14:25:50 -0400

    initial commit

---
+++ SudokuLab4/src/main/java/pkgGame/Sudoku.java
@@ -0,0 +1,513 @@
+package pkgGame;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Random;
+
+import pkgHelper.LatinSquare;
+
+/**
+ * Sudoku - This class extends LatinSquare, adding methods, constructor to
+ * handle Sudoku logic
+ * 
+ * @version 1.2
+ * @since Lab #2
+ * @author Bert.Gibbons
+ *
+ */
+public class Sudoku extends LatinSquare {
+
+	/**
+	 * 
+	 * iSize - the length of the width/height of the Sudoku puzzle.
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 */
+	private int iSize;
+
+	/**
+	 * iSqrtSize - SquareRoot of the iSize. If the iSize is 9, iSqrtSize will be
+	 * calculated as 3
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 */
+
+	private int iSqrtSize;
+
+	/**
+	 * Sudoku - for Lab #2... do the following:
+	 * 
+	 * set iSize If SquareRoot(iSize) is an integer, set iSqrtSize, otherwise throw
+	 * exception
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param iSize-
+	 *            length of the width/height of the puzzle
+	 * @throws Exception
+	 *             if the iSize given doesn't have a whole number square root
+	 */
+	public Sudoku(int iSize) throws Exception {
+
+		this.iSize = iSize;
+
+		double SQRT = Math.sqrt(iSize);
+		if ((SQRT == Math.floor(SQRT)) && !Double.isInfinite(SQRT)) {
+			this.iSqrtSize = (int) SQRT;
+		} else {
+			throw new Exception("Invalid size");
+		}
+
+		int[][] puzzle = new int[iSize][iSize];
+		super.setLatinSquare(puzzle);
+		FillDiagonalRegions();
+	}
+
+	/**
+	 * Sudoku - pass in a given two-dimensional array puzzle, create an instance.
+	 * Set iSize and iSqrtSize
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param puzzle
+	 *            - given (working) Sudoku puzzle. Use for testing
+	 * @throws Exception
+	 *             will be thrown if the length of the puzzle do not have a whole
+	 *             number square root
+	 */
+	public Sudoku(int[][] puzzle) throws Exception {
+		super(puzzle);
+		this.iSize = puzzle.length;
+		double SQRT = Math.sqrt(iSize);
+		if ((SQRT == Math.floor(SQRT)) && !Double.isInfinite(SQRT)) {
+			this.iSqrtSize = (int) SQRT;
+		} else {
+			throw new Exception("Invalid size");
+		}
+
+	}
+
+	/**
+	 * getPuzzle - return the Sudoku puzzle
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @return - returns the LatinSquare instance
+	 */
+	public int[][] getPuzzle() {
+		return super.getLatinSquare();
+	}
+
+	/**
+	 * getRegionNbr - Return region number based on given column and row
+	 * 
+	 * 
+	 * Example, the following Puzzle:
+	 * 
+	 * 0 1 2 3 <br>
+	 * 1 2 3 4 <br>
+	 * 3 4 1 2 <br>
+	 * 4 1 3 2 <br>
+	 * 
+	 * getRegionNbr(3,0) should return a value of 1
+	 * 
+	 * @param iCol - Given column number
+	 * @param iRow - Given row number
+	 * @version 1.3
+	 * @since Lab #3
+	 * 
+	 * @return - return region number based on given column and row
+	 */
+	public int getRegionNbr(int iCol, int iRow) {
+
+		int i = (iCol / iSqrtSize) + ((iRow / iSqrtSize) * iSqrtSize);
+
+		return i;
+	}
+
+	/**
+	 * getRegion - figure out what region you're in based on iCol and iRow and call
+	 * getRegion(int)<br>
+	 * 
+	 * Example, the following Puzzle:
+	 * 
+	 * 0 1 2 3 <br>
+	 * 1 2 3 4 <br>
+	 * 3 4 1 2 <br>
+	 * 4 1 3 2 <br>
+	 * 
+	 * getRegion(0,3) would call getRegion(1) and return [2],[3],[3],[4]
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param iCol
+	 *            given column
+	 * @param iRow
+	 *            given row
+	 * @return - returns a one-dimensional array from a given region of the puzzle
+	 */
+	public int[] getRegion(int iCol, int iRow) {
+
+		int i = (iCol / iSqrtSize) + ((iRow / iSqrtSize) * iSqrtSize);
+
+		return getRegion(i);
+	}
+
+	/**
+	 * getRegion - pass in a given region, get back a one-dimensional array of the
+	 * region's content<br>
+	 * 
+	 * Example, the following Puzzle:
+	 * 
+	 * 0 1 2 3 <br>
+	 * 1 2 3 4 <br>
+	 * 3 4 1 2 <br>
+	 * 4 1 3 2 <br>
+	 * 
+	 * getRegion(2) and return [3],[4],[4],[1]
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param r
+	 *            given region
+	 * @return - returns a one-dimensional array from a given region of the puzzle
+	 */
+
+	public int[] getRegion(int r) {
+
+		int[] reg = new int[super.getLatinSquare().length];
+
+		int i = (r % iSqrtSize) * iSqrtSize;
+		int j = (r / iSqrtSize) * iSqrtSize;
+		int iMax = i + iSqrtSize;
+		int jMax = j + iSqrtSize;
+		int iCnt = 0;
+
+		for (; j < jMax; j++) {
+			for (i = (r % iSqrtSize) * iSqrtSize; i < iMax; i++) {
+				reg[iCnt++] = super.getLatinSquare()[j][i];
+			}
+		}
+
+		return reg;
+	}
+
+	/**
+	 * isPartialSudoku - return 'true' if...
+	 * 
+	 * It's a LatinSquare If each region doesn't have duplicates If each element in
+	 * the first row of the puzzle is in each region of the puzzle At least one of
+	 * the elemnts is a zero
+	 * 
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @return true if the given puzzle is a partial sudoku
+	 */
+	public boolean isPartialSudoku() {
+
+		if (!super.isLatinSquare()) {
+			return false;
+		}
+
+		for (int k = 0; k < this.getPuzzle().length; k++) {
+
+			if (super.hasDuplicates(getRegion(k))) {
+				return false;
+			}
+
+			if (!hasAllValues(getRow(0), getRegion(k))) {
+				return false;
+			}
+		}
+
+		if (ContainsZero()) {
+			return false;
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * isSudoku - return 'true' if...
+	 * 
+	 * Is a partialSudoku Each element doesn't a zero
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @return - returns 'true' if it's a partialSudoku, element match (row versus
+	 *         column) and no zeros
+	 */
+	public boolean isSudoku() {
+
+		if (!isPartialSudoku()) {
+			return false;
+		}
+
+		if (ContainsZero()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * isValidValue - test to see if a given value would 'work' for a given column /
+	 * row
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param iCol
+	 *            puzzle column
+	 * @param iRow
+	 *            puzzle row
+	 * @param iValue
+	 *            given value
+	 * @return - returns 'true' if the proposed value is valid for the row and column
+	 */
+	public boolean isValidValue(int iRow,int iCol,  int iValue) {
+		
+		if (doesElementExist(super.getRow(iRow),iValue))
+		{
+			return false;
+		}
+		if (doesElementExist(super.getColumn(iCol),iValue))
+		{
+			return false;
+		}
+		if (doesElementExist(this.getRegion(iCol, iRow),iValue))
+		{
+			return false;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * PrintPuzzle This method will print the puzzle to the console (space between
+	 * columns, line break after row)
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 */
+	public void PrintPuzzle() {
+		for (int i = 0; i < this.getPuzzle().length; i++) {
+			System.out.println("");
+			for (int j = 0; j < this.getPuzzle().length; j++) {
+				System.out.print(this.getPuzzle()[i][j]);
+				if ((j + 1) % iSqrtSize == 0)
+					System.out.print(" ");
+			}
+			if ((i + 1) % iSqrtSize == 0)
+				System.out.println(" ");
+
+		}
+		System.out.println("");
+	}
+
+	/**
+	 * FillDiagonalRegions - After the puzzle is created, set the diagonal regions
+	 * with random values
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 */
+	private void FillDiagonalRegions() {
+
+		for (int i = 0; i < iSize; i = i + iSqrtSize) {
+			System.out.println("Filling region: " + getRegionNbr(i, i));
+			SetRegion(getRegionNbr(i, i));
+			ShuffleRegion(getRegionNbr(i, i));
+		}
+	}
+
+	/**
+	 * SetRegion - purpose of this method is to set the values of a given region
+	 * (they will be shuffled later)
+	 * 
+	 * Example, the following Puzzle start state:
+	 * 
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 
+	 * SetRegion(2) would transform the Puzzle to:<br>
+	 * 
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 1 2 0 0 <br>
+	 * 3 4 0 0 <br>
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 * @param r
+	 *            - Given region number
+	 */
+	private void SetRegion(int r) {
+		int iValue = 0;
+
+		iValue = 1;
+		for (int i = (r / iSqrtSize) * iSqrtSize; i < ((r / iSqrtSize) * iSqrtSize) + iSqrtSize; i++) {
+			for (int j = (r % iSqrtSize) * iSqrtSize; j < ((r % iSqrtSize) * iSqrtSize) + iSqrtSize; j++) {
+				this.getPuzzle()[i][j] = iValue++;
+			}
+		}
+	}
+
+	/**
+	 * SetRegion - purpose of this method is to set the values of a given region
+	 * (they will be shuffled later)
+	 * 
+	 * Example, the following Puzzle start state:
+	 * 
+	 * 1 2 0 0 <br>
+	 * 3 4 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 
+	 * ShuffleRegion(0) might transform the Puzzle to:<br>
+	 * 
+	 * 2 3 0 0 <br>
+	 * 1 4 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 * @param r
+	 *            - Given region number
+	 */
+	private void ShuffleRegion(int r) {
+		int[] region = getRegion(r);
+		shuffleArray(region);
+		int iCnt = 0;
+		for (int i = (r / iSqrtSize) * iSqrtSize; i < ((r / iSqrtSize) * iSqrtSize) + iSqrtSize; i++) {
+			for (int j = (r % iSqrtSize) * iSqrtSize; j < ((r % iSqrtSize) * iSqrtSize) + iSqrtSize; j++) {
+				this.getPuzzle()[i][j] = region[iCnt++];
+			}
+		}
+	}
+
+	/**
+	 * shuffleArray this method will shuffle a given one-dimension array
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 * @param ar
+	 *            given one-dimension array
+	 */
+	private void shuffleArray(int[] ar) {
+
+		Random rand = new SecureRandom();
+		for (int i = ar.length - 1; i > 0; i--) {
+			int index = rand.nextInt(i + 1);
+			// Simple swap
+			int a = ar[index];
+			ar[index] = ar[i];
+			ar[i] = a;
+		}
+	}
+	
+	public boolean isValidRowValue(int iRow, int iValue) {
+
+		return doesElementExist(getRow(iRow), iValue) ? false : true;
+	}
+	
+	public boolean isValidColumnValue(int iCol, int iValue) {
+		
+		return doesElementExist(getColumn(iCol), iValue) ? false : true;
+	}
+	
+	public boolean isValidRegionValue(int iRow, int iCol, int iValue) {
+		
+		return doesElementExist(getRegion(iCol, iRow), iValue) ? false : true;
+	}
+	
+	/*public isValidValue(int iRow, int iCol, int iValue) {
+	 * 	if (isValidRowValue(iRow, iValue) && isValidColumnValue(iCol, iValue) && isValidRegionValue(iRow, iCol, iValue) {
+	 *		return true;
+	 *} else {
+	 *		return false;
+	 *}
+	 *}
+	 * 
+	 */
+	
+	public ArrayList<Integer> validValues(int iRow, int iCol) {
+		
+		ArrayList<Integer> validValues = new ArrayList<Integer>();
+//		for (int i = 1; i <= iSize; i++) {
+//			validValues.add(i);
+// 		}
+//		
+//		for (int ii = 0; ii < iSize; ii++) {
+//			if (validValues.contains(getRow(iRow)[ii]) && getRow(iRow)[ii] != 0) {
+//				validValues.remove(getRow(iRow)[ii]);
+//			}
+//			if (validValues.contains(getColumn(iCol)[ii]) && getColumn(iCol)[ii] != 0) {
+//				validValues.remove(getColumn(iCol)[ii]);
+//			}
+//			if (validValues.contains(getRegion(iCol, iRow)[ii]) && getRegion(iCol, iRow)[ii] != 0) {
+//				validValues.remove(getRegion(iCol, iRow)[ii]);
+//			}
+//		}
+		
+		for (int i = 0; i < iSize; i++) {
+			if (!doesElementExist(getRow(i), i + 1) &&
+					!doesElementExist(getColumn(i), i + 1) &&
+					!doesElementExist(getRegion(i), i + 1)) {
+				validValues.add(i + 1);
+			}
+			//if (isValidValue(iRow, iCol, iValue))
+					//validValues.add(iValue)
+		}
+		
+		return validValues;
+	}
+	
+	/*FillREgions() steps:
+	 * 
+	 * iteration by row number first 
+	 * 		figure out remaining values for row
+	 * 		
+	 * 
+	 * REcursion:
+	 * 		if (iCol >= iSize) --> getRow()[iCol] != 0 (iteration through row until [row] isnt 0)
+	 * 		Would it be recursion per row or recursion within each row to determine the row (or both)
+	 * 		return true to go forward; return false to backtrack
+	 * 
+	 * for (iterate the row by col idxs)
+	 * 		if (row[idx] != 0)
+	 * 			if validValues.size() == 0
+	 * 				logic for changing values in existing row
+	 * 				Options:
+	 * 					restart entire row (720 options max per row based on 6 potential numbers per row to fill in)	
+	 * 						(720 ^ 9) max iterations possible for this method
+	 * 						
+	 * 					go back one col spot (ex: 4 --> 3) and change the value 
+	 * 						What if it still doesn't work?
+	 * 							determine variable (squaresBack) that increases on each failed attempt to fill out row
+	 * 							variable should only increase if all the possible values for a square have been used
+	 * 							variable resets once a row has been determined to be valid
+	 * 							if we use a randomize to pick the validValue from the list/ArrayList, how do prevent it from picking it again?
+	 * 								possibly remove the value when we use it (would cause issue when s8 has 2 possibilities and we need more than 2 tries)
+	 * 						What if it STILL doesn't work? (Every possibility for Row 2 does not work (more realistically row 6,7,8)
+	 * 
+	 * 					determine possible values for each square in each row
+	 * 						fill in square with lowest number of possibilities first?
+	 * 							would need to be stored in an array/Arraylist
+	 * 							would need to be sorted based on value which would make sense to be a map (java dict equivalent)
+	 * 						similar to option 1 but should drastically reduce number of possibilities to iterate through?
+	 * 						could combine this with option 2 for best efficiency
+	 * 					
+	 * 			else
+	 * 				add value from validValues(row, col)
+	 * 				(should be random)
+	 * 
+	 * 
+	 */
+}
